51944c298717e29b21383ffd39c05687308bccc4
David Long <longdn@dukes.jmu.edu>
2020-02-12 12:06:36 -0500

    file for pytest

---
+++ fizzbuzz_test.py
@@ -0,0 +1,17 @@
+#pylint: disable=unused-variable 
+"""Unit tests for Fizzbuzz"""
+
+#import the code to be tested
+from fizzbuzz1 import fizz
+
+def describe_a_fizzbuzz_program():
+    """A program to play the FizzBuzz game."""
+
+    def test_it_can_blow_smoke():
+        """Make sure our testing infrastructure is working"""
+        assert True == True
+        
+def can_determine_if_a_number_is_a_multiple_of_3():
+    """Checks to see if a given number is a multiple of 3."""
+    assert fizz(3) == True
+    assert fizz(2) == False
