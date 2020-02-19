0622f7ae12076c7e08e4442d4bfae60e3f4b57ec
David Long <longdn@dukes.jmu.edu>
2020-02-19 12:02:44 -0500

    changges meade in class

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -1,17 +1,33 @@
-#pylint: disable=unused-variable 
-"""Unit tests for Fizzbuzz"""
+# pylint: disable=unused-variable
+"""Unit tests for FizzBuzz."""
 
-#import the code to be tested
+# import the code to be tested
 from fizzbuzz1 import fizz
 
-def describe_a_fizzbuzz_program():
+#import the method needed to test for exceptions
+from pytest import raises
+
+def describe_a_fizzbuzz_program_that():
     """A program to play the FizzBuzz game."""
 
-    def test_it_can_blow_smoke():
-        """Make sure our testing infrastructure is working"""
-        assert True == True
-        
+def has_a_smoke_test():
+    """Make sure our testing infrastructure is working."""
+    assert True == True
+
+    def describes_a_fizz_funtion_that():
+
+    def throws_an_error_if_input_to_fizz_is_not_a_positive_integer_or_text():
+        with raises(TypeError) as err_info:
+            fizz() # pylint: disable=no-value-for-parameter
+        print(err_info)
+
         def can_determine_if_a_number_is_a_multiple_of_3():
-            """Checks to see if a given number is a multiple of 3."""
-            assert fizz(3) == True
-            assert fizz(2) == False
+        """Checks to see if a given number is a multiple of 3."""
+        assert fizz(3) == True       # multiple of 3
+        assert fizz(2) == False      # non-multiple of 3
+        assert fizz(3.5) == False    # non-integer
+        assert fizz(0) == True       # zero
+        assert fizz(-3) == True      # negative multiple of 3
+        assert fizz(-4) == False     # negative non-multiple of 3
+        assert fizz('buzz') == False # non-numeric input
+        assert fizz() == False       # no input
